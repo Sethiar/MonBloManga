d6df8b00da2bd8c884b8ebaf50c04bdbdb444db8
Lefetey Arnaud <alefetey123@gmail.com>
2024-05-29 13:34:11 +0200

    Mise à jour fichiers templates et app.py

--- app.py
+++ app.py
@@ -4,12 +4,45 @@ from datetime import datetime
 
 from app.create_app import create_app
 
-from flask import render_template
+from flask import render_template, redirect, url_for
 
 from Models.author import Author
 from Models.articles import Article
 
+from login_manager import login_manager
 app = create_app()
+
+
+# Route renvoyant l'erreur 404.
+@app.errorhandler(404)
+def page_not_found(error):
+    """
+    Renvoie une page d'erreur 404 en cas de page non trouvée.
+
+    Args :
+        error : L'erreur qui a déclenché la page non trouvée.
+
+    Returns :
+        La page d'erreur 404.
+    """
+    return render_template("Error/404.html"), 404
+
+
+# Route permettant de renvoyant l'utilisateur vers les bons moyens d'authentification.
+@login_manager.unauthorized_handler
+def unauthorized():
+    """
+    Fonction exécutée lorsque l'utilisateur tente d'accéder à une page nécessitant une connexion,
+    mais n'est pas authentifié. Redirige l'utilisateur vers la page "connexion_requise".
+
+    Cette fonction est utilisée pour gérer les tentatives d'accès non autorisé à des pages nécessitant une connexion.
+    Lorsqu'un utilisateur non authentifié essaie d'accéder à une telle page, cette fonction est appelée et redirige
+    l'utilisateur vers la page "connexion_requise" où il peut se connecter.
+
+    Returns:
+        Redirige l'utilisateur vers la page "connexion_requise".
+    """
+    return redirect(url_for('functional.connexion_requise'))
 
 
 # Route permettant d'accéder à l'accueil du blog.
