5a0890efa2abd5856a012ceaf5f0435a4b879e3d
Lefetey Arnaud <alefetey123@gmail.com>
2024-05-29 13:31:06 +0200

    Mise à jour des fichiers dans app

--- app/functional/routes.py
+++ app/functional/routes.py
@@ -3,14 +3,13 @@ Code permettant de définir les routes concernant les fonctions cachées, erreur
 politique du blog.
 """
 
-from app.functional import functional_bp
-
 import uuid
 
 from flask import render_template, redirect, url_for, session
 from flask_login import current_user
 
 from login_manager import login_manager
+from app.functional import functional_bp
 
 
 def generate_unique_id():
@@ -19,53 +18,6 @@ def generate_unique_id():
         str: L'identifiant unique est généré.
         """
     return str(uuid.uuid4())
-
-
-# Route renvoyant l'erreur 401.
-@functional_bp.errorhandler(401)
-def acces_no_autorise(error):
-    """
-    Renvoie une page d'erreur 401 en cas d'accès non autorisé.
-
-    Args :
-        error : L'erreur qui a déclenché l'accès non autorisé.
-
-    Returns :
-        La page d'erreur 401.
-    """
-    return render_template("Error/401.html"), 401
-
-
-# Route renvoyant l'erreur 404.
-@functional_bp.errorhandler(404)
-def page_not_found(error):
-    """
-    Renvoie une page d'erreur 404 en cas de page non trouvée.
-
-    Args :
-        error : L'erreur qui a déclenché la page non trouvée.
-
-    Returns :
-        La page d'erreur 404.
-    """
-    return render_template("Error/404.html"), 404
-
-
-# Route permettant de renvoyant l'utilisateur vers les bons moyens d'authentification.
-@login_manager.unauthorized_handler
-def unauthorized():
-    """
-    Fonction exécutée lorsque l'utilisateur tente d'accéder à une page nécessitant une connexion,
-    mais n'est pas authentifié. Redirige l'utilisateur vers la page "connexion_requise".
-
-    Cette fonction est utilisée pour gérer les tentatives d'accès non autorisé à des pages nécessitant une connexion.
-    Lorsqu'un utilisateur non authentifié essaie d'accéder à une telle page, cette fonction est appelée et redirige
-    l'utilisateur vers la page "connexion_requise" où il peut se connecter.
-
-    Returns:
-        Redirige l'utilisateur vers la page "connexion_requise".
-    """
-    return redirect(url_for('functional.connexion_requise'))
 
 
 # Route permettant à l'utilisateur de bien se connecter au blog.
