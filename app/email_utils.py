b1960b53725799edb515f877e26010789f14e71e
Lefetey Arnaud <alefetey123@gmail.com>
2024-07-04 19:36:28 +0200

    Ajout docstring à email_utils.py.

--- app/email_utils.py
+++ app/email_utils.py
@@ -1,12 +1,13 @@
 from threading import Thread
-from flask import current_app
 
 
 def send_async_email(app, msg):
     """
+    Envoie un email de manière asynchrone en utilisant le contexte de l'application Flask.
 
-    :param app:
-    :param msg:
+    Args:
+        app (Flask): L'instance de l'application Flask.
+        msg (Message): L'objet Message contenant les détails de l'email à envoyer.
     """
     with app.app_context():
         app.extensions['mail'].send(msg)
@@ -14,8 +15,10 @@ def send_async_email(app, msg):
 
 def send_email_in_background(app, msg):
     """
+    Lance l'envoi d'un email dans un thread séparé.
 
-    :param app:
-    :param msg:
+    Args:
+        app (Flask): L'instance de l'application Flask.
+        msg (Message): L'objet Message contenant les détails de l'email à envoyer.
     """
     Thread(target=send_async_email, args=(app, msg)).start()
